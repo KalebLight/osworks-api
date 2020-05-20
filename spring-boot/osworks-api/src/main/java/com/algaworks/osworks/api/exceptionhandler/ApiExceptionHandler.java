4b8d4470a6f4117429902a44da9e0007431a70a7
KalebLight <kaleb.luiz@academico.ifrn.edu.br>
2020-05-20 10:31:29 -0300

    Finalizando tratamento de exceções

--- spring-boot/osworks-api/src/main/java/com/algaworks/osworks/api/exceptionhandler/ApiExceptionHandler.java
+++ spring-boot/osworks-api/src/main/java/com/algaworks/osworks/api/exceptionhandler/ApiExceptionHandler.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -20,9 +21,14 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
 	
 		var campos = new ArrayList<Problema.Campo>();
+		
 		for(ObjectError error : ex.getBindingResult().getAllErrors()) {
-			String mensagem = error.getDefaultMessage();
+			String nome = ((FieldError)error).getField();			
+			String mensagem = error.getDefaultMessage();			
+			campos.add(new Problema.Campo(nome, mensagem));
 		}
+		
+			
 		var problema = new Problema();
 		problema.setStatus(status.value());
 		problema.setTitulo("Um ou mais campos estão inválidos, "
--- spring-boot/osworks-api/src/main/java/com/algaworks/osworks/api/exceptionhandler/Problema.java
+++ spring-boot/osworks-api/src/main/java/com/algaworks/osworks/api/exceptionhandler/Problema.java
@@ -14,6 +14,13 @@ public class Problema {
 		private String nome;
 		private String mensagem;
 		
+		public Campo(String nome, String mensagem) {
+			super();
+			this.nome = nome;
+			this.mensagem = mensagem;
+		}
+		
+		
 		public String getNome() {
 			return nome;
 		}
