182544656e3fe7e79a8e207f3a5ae1af5eb19276
KalebLight <kaleb.luiz00@gmail.com>
2020-06-09 10:08:05 -0300

    Implementando representation model da entrada de dados (OrdemServico)

--- spring-boot/osworks-api/src/main/java/com/algaworks/osworks/api/controller/OrdemServicoController.java
+++ spring-boot/osworks-api/src/main/java/com/algaworks/osworks/api/controller/OrdemServicoController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.osworks.api.model.OrdemServicoInput;
 import com.algaworks.osworks.api.model.OrdemServicoModel;
 import com.algaworks.osworks.domain.model.OrdemServico;
 import com.algaworks.osworks.domain.repository.OrdemServicoRepository;
@@ -38,7 +39,8 @@ public class OrdemServicoController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public OrdemServicoModel criar(@Valid @RequestBody OrdemServico ordemServico) {
+	public OrdemServicoModel criar(@Valid @RequestBody OrdemServicoInput ordemServicoInput) {
+		OrdemServico ordemServico = toEntity(ordemServicoInput);
 		return toModel(gestaoOrdemServico.criar(ordemServico));	
 	}
 	
@@ -59,11 +61,16 @@ public class OrdemServicoController {
 		}
 	}
 	
-	public OrdemServicoModel toModel(OrdemServico ordemServico) {
+	private OrdemServicoModel toModel(OrdemServico ordemServico) {
 		return modelMapper.map(ordemServico, OrdemServicoModel.class);
 	}
 	
-	public List<OrdemServicoModel> toCollectionModel(List<OrdemServico> ordensServico){
+	private List<OrdemServicoModel> toCollectionModel(List<OrdemServico> ordensServico){
 		return ordensServico.stream().map(ordemServico -> toModel(ordemServico)).collect(Collectors.toList());		
 	}
+	
+	private OrdemServico toEntity(OrdemServicoInput ordemServicoInput) {
+		return modelMapper.map(ordemServicoInput, OrdemServico.class);
+	}
+	
 }
