3ddce2a05c5324e6d5be4a3fdcd5b8c0ab9bf39f
KalebLight <kaleb.luiz00@gmail.com>
2020-06-16 18:46:23 -0300

    Implementando a listagem de Comentários

--- spring-boot/osworks-api/src/main/java/com/algaworks/osworks/api/controller/ComentarioController.java
+++ spring-boot/osworks-api/src/main/java/com/algaworks/osworks/api/controller/ComentarioController.java
@@ -1,10 +1,14 @@
 package com.algaworks.osworks.api.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.validation.Valid;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,9 +16,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.osworks.api.exceptionhandler.EntidadeNaoEncontradaException;
 import com.algaworks.osworks.domain.model.Comentario;
 import com.algaworks.osworks.domain.model.ComentarioInput;
 import com.algaworks.osworks.domain.model.ComentarioModel;
+import com.algaworks.osworks.domain.model.OrdemServico;
+import com.algaworks.osworks.domain.repository.OrdemServicoRepository;
 import com.algaworks.osworks.domain.service.GestaoOrdemServicoService;
 
 @RestController
@@ -24,8 +31,20 @@ public class ComentarioController {
 	@Autowired
 	private GestaoOrdemServicoService gestaoOrdemServico;
 	@Autowired
+	private OrdemServicoRepository ordemServicoRepository;
+	@Autowired
 	private ModelMapper modelMapper;
 	
+	@GetMapping
+	public List<ComentarioModel> listar(@PathVariable Long ordemServicoId){
+		OrdemServico ordemServico = ordemServicoRepository.findById(ordemServicoId).orElseThrow(() -> new EntidadeNaoEncontradaException("Ordem de serviço não encontrada"));
+		
+		return toCollectionModel(ordemServico.getComentarios());		
+	}
+	
+	private List<ComentarioModel> toCollectionModel(List<Comentario> comentarios){
+		return comentarios.stream().map(comentario -> toModel(comentario)).collect(Collectors.toList());
+	}
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
